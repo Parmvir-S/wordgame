534da452e276c42875282f5b2ac6d1f7dd2eaec7
Parmvir-S <parmvir.shergill@gmail.com>
2023-01-14 22:38:26 -0800

    changed config

--- src/utils/socketConfig.js
+++ src/utils/socketConfig.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io("https://wordgameps.herokuapp.com/");
+const socket = io("https://web-production-a829.up.railway.app/");
 
 // const socket = io("http://localhost:8000/");
 
